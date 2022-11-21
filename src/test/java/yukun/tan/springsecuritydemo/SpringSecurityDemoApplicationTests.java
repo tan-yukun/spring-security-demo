bd9b7f9946155b65914bd79ad7c688e9095782d8
tanyukun <tan_yukun@outlook.com>
2022-11-21 15:49:17 +0800

    init commit

---
+++ src/test/java/yukun/tan/springsecuritydemo/SpringSecurityDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package yukun.tan.springsecuritydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
